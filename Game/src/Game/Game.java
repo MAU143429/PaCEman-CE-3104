9a3553f827046ba948c3a0176835dae1c33dcfc6
Yendry <yen.badilla@gmail.com>
2021-11-06 16:31:34 -0600

    Initial Game GUI

---
+++ Game/src/Game/Game.java
@@ -0,0 +1,46 @@
+package Game;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+/**
+ * Main game classs, it constructs the board game
+ */
+public class Game extends JPanel implements ActionListener{
+    private Dimension dimension;
+    private final Font smallFont = new Font("Monospaced", Font.BOLD, 14);
+    private boolean inGame = false; //checks if game is running
+    private boolean dying = false;
+
+    private final int BLOCK_SIZE = 24;
+    private final int N_BLOCKS = 15;
+    private final int SCREEN_SIZE = N_BLOCKS * BLOCK_SIZE;
+    private final int MAX_GHOSTS = 12;
+    private final int PACMAN_SPEED = 6;
+
+    private int N_GHOSTS = 6;
+    private int lives, score;
+    private int[] dx, dy;
+    private int[] ghost_x, ghost_y, ghost_dx, ghost_dy, ghostSpeed;
+
+    private Image heart, ghost;
+    private Image up, down, left, right;
+
+    private int pacman_x, pacman_y, pacmand_x, pacmand_y;
+    private int req_dx, req_dy;
+
+
+    public Game() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+}
