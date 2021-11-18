c256b4bb2980b1286cef78f95ec857fa30bbafaf
MAU <mau141226@gmail.com>
2021-11-18 14:50:22 -0600

    [Update] Server .exe uploaded

---
+++ Server/cmake-build-debug/CMakeFiles/Server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Server.dir/Server/main.c.obj"
+  "Server.exe"
+  "Server.exe.manifest"
+  "Server.lib"
+  "Server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
